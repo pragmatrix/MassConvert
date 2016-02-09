f1e954c0ecafd2a9227e103d48b3331f2d1efe25
Armin Sander <armin@replicator.org>
2016-02-09 07:49:34 +0100

    provide a context with each action error

--- MassConvert.Core/Player.fs
+++ MassConvert.Core/Player.fs
@@ -8,35 +8,53 @@ open Director
 
 /// Runs actions.
 module Player = 
+    open System.Diagnostics
 
     type ActionResult = 
-        | ActionError of exn
+        | ActionError of string * exn
         | ActionSuccess
 
-    let private act (f: unit -> unit) = 
+    let private act context (f: unit -> unit) = 
         try
             f()
             ActionSuccess
         with e ->
-            ActionError e
+            ActionError (context(), e)
     
     let private args (inputFile: Path) (outputFile: Path) = 
         ["inputFile", inputFile.value; "outputFile", outputFile.value]
         |> Template.Arguments.ofList
 
     let private runCommand (commandLine: string) = 
-        ()        
+        let si = ProcessStartInfo()      
+        si.UseShellExecute <- false
+        si.Arguments <- commandLine
+        match Process.Start(si) with
+        | null -> failwith "failed to start process (null)"
+        | p ->
+        use proc = p
+        proc.WaitForExit()
+        if proc.ExitCode <> 0 then
+            failwithf "command failed with exit code %d" proc.ExitCode
+        else
+        ()
+
 
     let fileSystemPlayer (commandTemplate: string) (action: Action) : ActionResult = 
-        fun () ->
-            match action with
-            | TryRemoveDir p -> Directory.Delete(p.value)
-            | TryRemoveFile p -> File.Delete(p.value)
-            | ConvertCommand (inputFile, outputFile) ->
-                let args = args inputFile outputFile
-                let commandLine = args.format commandTemplate
+        match action with
+        | TryRemoveDir p -> 
+            fun () -> Directory.Delete(p.value)
+            |> act (fun () -> sprintf "rmdir %s" p.value)
+        | TryRemoveFile p -> 
+            fun () -> File.Delete(p.value)
+            |> act (fun () -> sprintf "rmfile %s" p.value)
+        | ConvertCommand (inputFile, outputFile) ->
+            // if template formatting fails, we want a huge crash!
+            let args = args inputFile outputFile
+            let commandLine = args.format commandTemplate
+            fun () ->
                 runCommand commandLine
-        |> act
+            |> act (fun() -> "> " + commandLine)
 
     let dryPlayer (commandTemplate: string) (action: Action) : string = 
         match action with
