15b5cabe73bc7e90e1d232438ac437d2ebdbce7a
Armin Sander <armin@replicator.org>
2016-02-10 18:09:12 +0100

    more usability

--- MassConvert/Program.fs
+++ MassConvert/Program.fs
@@ -131,7 +131,7 @@ module Main =
             | Verbose -> f()
             | Silent -> ()
 
-        ifVerbose (fun () -> printfn "==== scanning '%s'" ("/" + path.string))
+        ifVerbose (fun () -> printfn "==== scanning %s" ("/" + path.string))
 
         let actions = 
             JobBuilder.forRelativePath config path
@@ -142,7 +142,12 @@ module Main =
         | [||] ->
             ifVerbose (fun () -> printfn "==== nothing to do")
         | actions ->
-            ifVerbose (fun () -> printfn "==== processing %d change(s)" actions.Length)
+            ifVerbose (fun () -> 
+                let info = 
+                    match startupArgs.mode with
+                    | DryRun -> "showing"
+                    | Convert -> "processing"
+                printfn "==== %s %d change(s)" info actions.Length)
             actions |> processActions startupArgs config
 
     let processWatchEvent (startupArgs: StartupArguments) (config: Configuration) (event: Watcher.WatchEvent) = 
